8872a671786a6d647144daba4203faa5deb3c7f1
venktshtiwari <venkatesh.tiwari.met25@itbhu.ac.in>
2026-06-11 16:38:31 +0530

    Fix project structure and remove stray main binary

---
+++ build/CMakeFiles/grep.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/venkatesh/codecrafters-grep-cpp/grep-cpp/src/main.cpp" "CMakeFiles/grep.dir/src/main.cpp.o" "gcc" "CMakeFiles/grep.dir/src/main.cpp.o.d"
+  "" "grep" "gcc" "CMakeFiles/grep.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/grep.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for grep.
+# This may be replaced when dependencies are built.
